c6a3e2df70caba2590860353c176da05fe175f4a
A-Salty-Fish <1309032417@qq.com>
2022-03-15 16:21:46 +0800

    feat: test message entity

---
+++ src/main/java/demo/TestEntity.java
@@ -0,0 +1,36 @@
+package demo;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/3/15 16:19
+ */
+@Data
+public class TestEntity {
+
+    private String name;
+
+    private String name2;
+
+    public Long longNum;
+
+    public Long longNum2;
+
+    public Integer intNum;
+
+    public Integer intNum2;
+
+    public Double doubleNum;
+
+    public Double doubleNum2;
+
+    public LocalDate localDate;
+
+    public LocalDateTime localDateTime;
+}
