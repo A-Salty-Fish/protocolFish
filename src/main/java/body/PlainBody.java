a9bac4afb8115af53935f4fda590a4e9b24d63de
A-Salty-Fish <1309032417@qq.com>
2022-03-15 14:26:52 +0800

    feat: init header and body

---
+++ src/main/java/body/PlainBody.java
@@ -0,0 +1,11 @@
+package body;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/3/15 14:26
+ */
+
+public class PlainBody {
+}
---
+++ src/main/java/header/PlainHeader.java
@@ -0,0 +1,11 @@
+package header;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/3/15 14:24
+ */
+
+public class PlainHeader {
+}
